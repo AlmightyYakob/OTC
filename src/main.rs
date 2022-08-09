dd974d680eba877f14b92b065c2523a4265342ae
Jacob Nesbitt <jjnesbitt2@gmail.com>
2022-08-09 18:47:01 -0400

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::Parser;
+use std::{fs, path::PathBuf};
+use vue_sfc::{Block, BlockName, Section};
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The paths to convert
+    #[clap(name = "PATHS", parse(from_os_str), required = true)]
+    paths: Vec<PathBuf>,
+
+    #[clap(
+        short,
+        long,
+        help = "Recurse into directories.",
+        default_value_t = false
+    )]
+    recursive: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("{:?}", args.paths);
+
+    let path = &args.paths[0];
+    let data = fs::read_to_string(path).expect("Unable to read file");
+
+    let sfc = vue_sfc::parse(&data).unwrap();
+    for section in sfc {
+        match section {
+            Section::Block(Block {
+                name,
+                attributes,
+                content,
+            }) => {
+                println!(
+                    "Got a block named `{}` with {} attributes, content is {} bytes long.",
+                    name,
+                    attributes.len(),
+                    content.len()
+                );
+
+                if name.as_str() == "script" {
+                    println!("{:?}", attributes);
+                }
+            }
+            _ => {
+                panic!("This shouldn't happen")
+            }
+        }
+    }
 }
